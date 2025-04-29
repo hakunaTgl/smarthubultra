8fb26487eaac1847f539ed4f211693edcd85d8b3
Hakuna <98fenwickt@gmail.com>
2025-04-29 15:40:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,77 @@
+const CACHE_NAME = 'smarthubultra-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/manifest.json',
+  '/js/main.js',
+  '/js/auth.js',
+  '/js/bots.js',
+  '/js/dashboard.js',
+  '/js/inspiration.js',
+  '/js/builder.js',
+  '/js/workshop.js',
+  '/js/editor.js',
+  '/js/playground.js',
+  '/js/creators.js',
+  '/js/collab.js',
+  '/js/voice.js',
+  '/js/analytics.js',
+  '/js/account.js',
+  '/js/manual.js',
+  '/js/boss.js',
+  '/js/holoGuide.js',
+  '/js/arControl.js',
+  '/js/behavioralDNA.js',
+  '/js/predictiveTasks.js',
+  '/js/notifications.js',
+  '/js/utils.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('smarthubultra-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/js/main.js',
-        '/js/auth.js',
-        '/js/dashboard.js',
-        '/js/bots.js',
-        '/js/inspiration.js',
-        '/js/builder.js',
-        '/js/workshop.js',
-        '/js/editor.js',
-        '/js/playground.js',
-        '/js/creators.js',
-        '/js/collab.js',
-        '/js/voice.js',
-        '/js/analytics.js',
-        '/js/account.js',
-        '/js/manual.js',
-        '/js/boss.js',
-        '/js/holoGuide.js',
-        '/js/arControl.js',
-        '/js/behavioralDNA.js',
-        '/js/predictiveTasks.js',
-        '/js/notifications.js',
-        '/js/utils.js'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(networkResponse => {
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+            return networkResponse;
+          }
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return networkResponse;
+        });
+      })
   );
 });
