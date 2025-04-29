8ed5da6ee911c9d48c6287a2b39e7e59f5fc5d82
Hakuna <98fenwickt@gmail.com>
2025-04-29 14:41:12 -0400

    Update manual.js

--- js/manual.js
+++ js/manual.js
@@ -1,1 +1,10 @@
+import { showToast, speak } from './utils.js';
 
+export async function loadManual() {
+  try {
+    speak('Welcome to the User Manual! Learn how to use Smart Hub Ultra.');
+  } catch (error) {
+    showToast(`Failed to load Manual: ${error.message}`);
+    console.error('Manual Error:', error);
+  }
+}
